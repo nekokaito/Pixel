689a22c9b80f59c429b30c3c2f748a14e3cbc915
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-04-26 19:24:43 +0600

    Footer Setup and Font Installed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,9 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
+  themes: [
+    "light",
+    "dracula",
+  ],
   plugins: [require("daisyui")], 
 }
