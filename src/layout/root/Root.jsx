db85816d76ab4bb98b658557f46535eea114f9b6
Siddiq Sazzad <siddiqsazzad001@gmail.com>
2024-04-30 13:46:43 +0600

    Project completed

--- src/layout/root/Root.jsx
+++ src/layout/root/Root.jsx
@@ -2,22 +2,25 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../navbar/NavBar";
 import Footer from "../footer/Footer";
 import { Toaster } from "react-hot-toast";
+import ParticlesComponent from "../../components/ParticlesComponent";
 
 const Root = () => {
     return (
         <>
-        <div className="sticky top-0 z-10">
+        <div className="sticky top-0 z-20">
         <NavBar></NavBar>
         </div>
          
          <Toaster></Toaster>
-        
-           <div className="container mx-auto min-h-screen">
+           <ParticlesComponent id="particles"></ParticlesComponent>
+           <div className="relative z-10 container mx-auto min-h-screen">
 
            <Outlet></Outlet>
            </div>
-           
+           <div className="relative z-10">
            <Footer></Footer>
+           </div>
+           
          
         </>
     );
